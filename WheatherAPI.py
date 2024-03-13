b3766049d87ea1b759f3b99078382556c22dbced
Filippo <148588396+StoccoFilippo@users.noreply.github.com>
2024-03-13 22:36:19 +0900

    Update WheatherAPI.py

--- WheatherAPI.py
+++ WheatherAPI.py
@@ -1,11 +1,8 @@
 import requests
-
 import json
-
 import datetime
 
-
-# import the list of the different provinces
+# Import list of provinces
 with open(
     "c:\\Users\\Filippo\\Desktop\\Python\\Data\\province.json", "r"
 ) as provinces_list:
@@ -13,6 +10,7 @@ with open(
 
 print("Enter one of the following provinces")
 
+# Display the list of provinces
 count = 0
 x = True
 provinces_list2 = []
@@ -21,24 +19,29 @@ for province in provinces.items():
     provinces_list2.append(province[0])
     print("{0}. {1}".format(count, province[0]))
 
+# Prompt user to input a province
 while x == True:
     state = (input("Insert the state you are interested in")).lower().capitalize()
     try:
+        # Check if the input province is in the list of provinces
         if state not in provinces_list2:
             raise ValueError("Provincia non presente nella lista.")
 
+        # Get current hour and date
         hour = datetime.datetime.now().strftime("%H")
         date = datetime.datetime.now().strftime("%D")
-
         Y = datetime.datetime.now().strftime("%Y")
         M = datetime.datetime.now().strftime("%m")
         D = datetime.datetime.now().strftime("%d")
 
+        # Format date in ISO format
         date_ISO = "{0}-{1}-{2}".format(Y, M, D)
-        date_ISO
+
+        # Set start and end dates for the request
         start_day = date_ISO
         end_day = date_ISO
 
+        # Iterate through provinces to find the selected province and set parameters
         for province in provinces.items():
             if province[0] == state:
                 params = {
@@ -50,6 +53,7 @@ while x == True:
                     "end_day": end_day,
                 }
 
+        # Construct URL for API request
         url = str(
             "https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&current=temperature_2m,apparent_temperature&hourly=temperature_2m,apparent_temperature&start_date={2}&end_date={3}".format(
                 params["latitude"],
@@ -59,10 +63,12 @@ while x == True:
             )
         )
         print(url)
-        results = requests.get(url)
 
+        # Get results from API
+        results = requests.get(url)
         results_json = results.json()
 
+        # Extract temperature data for the current hour
         counter = 0
         temperature_2m = 0
         apparent_temperature = 0
@@ -70,12 +76,14 @@ while x == True:
             if counter == int(hour):
                 temperature_2m = n
             counter += 1
+
         counter = 0
         for n in results_json["hourly"]["apparent_temperature"]:
             if counter == int(hour):
                 apparent_temperature = n
             counter += 1
 
+        # Print temperature information
         print(
             "The current temperature in {0} is {1} C° while the apparent temperature is {2} C°".format(
                 state, temperature_2m, apparent_temperature
